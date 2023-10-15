67a462074519888f93231dd8edc016f15ff8a3cf
hamuAOD <hamu.555to666@gmail.com>
2023-10-15 23:39:24 +0900

    [neovim] fix treesitter

--- nvim/lua/hamu/plugins/treesitter.lua
+++ nvim/lua/hamu/plugins/treesitter.lua
@@ -3,7 +3,28 @@ return {
   build = ":TSUpdate",
   config = function()
     require('nvim-treesitter.configs').setup {
-      ensure_installed = 'all',
+      ensure_installed = {
+        "bash",
+        "c",
+        "css",
+        "diff",
+        "dockerfile",
+        "git_config",
+        "gitignore",
+        "go",
+        "html",
+        "ini",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "python",
+        "rust",
+        "toml",
+        "typescript",
+        "vim",
+        "yaml"
+      },
       highlight = {
         enable = true,
       },
