a50cfbcd9ab89d9579b69fd5332f600b05896618
hamuAOD <hamu.555to666@gmail.com>
2023-10-24 02:17:47 +0900

    [neovim-mason] fix

--- nvim/lua/hamu/plugins/mason.lua
+++ nvim/lua/hamu/plugins/mason.lua
@@ -26,5 +26,25 @@ return {
         "vls",
       }
     }
+    local mason_lspconfig = require("mason-lspconfig")
+    local on_attach = function(_, bufnr)
+      vim.api.nvim_buf_set_option(bufnr, "formatexpr",
+      "v:lua.vim.lsp.formatexpr(#{timeout_ms:250})")
+      -- _G.lsp_onattach_func(i, bufnr)
+    end
+    mason_lspconfig.setup_handlers({
+      function(server_name)
+        local opts = {
+          on_attach = on_attach,
+          settings = {
+            ["omniSharp"] = {
+              useGlobalMono = "always"
+            }
+          },
+        }
+        require("lspconfig")[server_name].setup(opts)
+      end,
+    })
+    vim.cmd("LspStart") -- 初回起動時はBufEnterが発火しない
   end,
 }
