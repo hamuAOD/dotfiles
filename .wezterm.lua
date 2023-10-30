53c56077812aa45e8fe859d1c7db068b80450e64
hamuAOD <hamu.555to666@gmail.com>
2023-10-31 03:16:14 +0900

    [wezterm] update

--- .wezterm.lua
+++ .wezterm.lua
@@ -26,7 +26,7 @@ config.initial_cols = 130
 config.initial_rows = 50
 config.font = wezterm.font('HackGen35 Console NF', { weight = 'Bold', italic = false })
 config.font_size = 13
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.94
 config.macos_window_background_blur = 12
 config.color_scheme = 'Dracula'
 -- config.treat_east_asian_ambiguous_width_as_wide = true
@@ -39,7 +39,7 @@ config.background = {
     },
     width = '100%',
     repeat_x = 'NoRepeat',
-    opacity = 0.21,
+    opacity = 0.25,
     --attachment = { Parallax = 0.1 },
   },
 }
@@ -52,8 +52,7 @@ config.window_background_gradient = {
     -- Red
     -- '#240a07', '#54211c', '#391f1f',
     -- Grey
-    -- '#0f0f0f', '#212121', '#141414',
-    '#0c031c', '#1e252e', '#111821',
+    '#0f1113', '#1a1c1e', '#141618',
   },
   -- preset = "Warm",
   interpolation = 'Linear',
