b0e3ed1927743d6b43b199dfb9138580efd49b58
hamuAOD <hamu.555to666@gmail.com>
2024-05-07 11:52:40 +0900

    del nvim-insx

--- nvim/lua/hamu/plugins/lexima.lua
+++ nvim/lua/hamu/plugins/lexima.lua
@@ -1,5 +1,6 @@
 return{
   'cohama/lexima.vim',
+  -- 'hrsh7th/nvim-insx', -- deleted by ERROR
   event = { "BufRead", "BufNewFile" },
   config = function()
     vim.cmd([[call lexima#add_rule({'char': '<', 'input_after': '>'})]])
