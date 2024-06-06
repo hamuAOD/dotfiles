610648a670b39188a53de37207a375edcca29dd1
hamuAOD <hamu.555to666@gmail.com>
2024-06-06 11:06:52 +0900

    another dracula

--- nvim/lua/hamu/plugins/colorscheme.lua
+++ nvim/lua/hamu/plugins/colorscheme.lua
@@ -23,10 +23,21 @@ return {
   --   -- vim.cmd 'autocmd ColorScheme * highlight Normal ctermbg=none',
   --   -- vim.cmd 'autocmd ColorScheme * highlight LineNr ctermbg=none',
   -- },
+  -- {
+  --   "Mofiqul/dracula.nvim",
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function ()
+  --     local dracula = require 'dracula'
+  --     dracula.setup()
+  --     vim.cmd.colorscheme 'dracula'
+  --   end,
+  -- },
   {
-    "Mofiqul/dracula.nvim",
+    "binhtran432k/dracula.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
+    opts = {},
     config = function ()
       local dracula = require 'dracula'
       dracula.setup()
