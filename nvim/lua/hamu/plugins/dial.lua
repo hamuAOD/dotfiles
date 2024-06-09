b9c8069928de7dfde37fb351ba4a615c8332e0d2
hamuAOD <hamu.555to666@gmail.com>
2024-06-10 01:35:16 +0900

    add rule

--- nvim/lua/hamu/plugins/dial.lua
+++ nvim/lua/hamu/plugins/dial.lua
@@ -11,6 +11,14 @@ return {
         augend.integer.alias.hex,
         augend.date.alias["%Y/%m/%d"],
         augend.constant.alias.bool,
+        augend.date.new{
+          pattern = "%Y年%m月%d日",
+          default_kind = "day",
+          -- if true, it does not match dates which does not exist, such as 2022/05/32
+          only_valid = true,
+          -- if true, it only matches dates with word boundary
+          word = false,
+        },
       },
     }
 
@@ -21,5 +29,11 @@ return {
     keymap.set('n', '<C-x>', '<Plug>(dial-decrement)', NS)
     keymap.set('x', '<C-a>', '<Plug>(dial-increment)', NS)
     keymap.set('x', '<C-x>', '<Plug>(dial-decrement)', NS)
+    vim.keymap.set("n", "g<C-a>", function()
+      require("dial.map").manipulate("increment", "gnormal")
+    end)
+    vim.keymap.set("n", "g<C-x>", function()
+      require("dial.map").manipulate("decrement", "gnormal")
+    end)
   end,
 }
