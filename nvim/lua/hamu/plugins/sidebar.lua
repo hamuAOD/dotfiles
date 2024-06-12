46530a8ea275ab04864bc618c7ce255c9dbb4bac
hamuAOD <hamu.555to666@gmail.com>
2024-06-13 00:09:58 +0900

    add buffers

--- nvim/lua/hamu/plugins/sidebar.lua
+++ nvim/lua/hamu/plugins/sidebar.lua
@@ -6,7 +6,22 @@ return{
   config = function ()
     require("sidebar-nvim").setup({
       side = "right",
-      sections = { "datetime", "git", "symbols", "diagnostics" },
+      datetime = {
+        icon = "",
+        format = "%a %b %d, %H:%M",
+        clocks = {
+            { name = "local" }
+        }
+      },
+      buffers = {
+        icon = "",
+        ignored_buffers = {},     -- ignore buffers by regex
+        sorting = "id",           -- alternatively set it to "name" to sort by buffer name instead of buf id
+        show_numbers = true,      -- whether to also show the buffer numbers
+        ignore_not_loaded = true, -- whether to ignore not loaded buffers
+        ignore_terminal = true,   -- whether to show terminal buffers in the list
+      },
+      sections = { "datetime", "buffers", "git", "symbols", "diagnostics" },
     })
 
     local NS = { noremap = true, silent = true }
