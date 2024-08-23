71f06b3c874c30ae4d5744c937d0c74c978f487c
hamuAOD <hamu.555to666@gmail.com>
2024-08-23 12:04:59 +0900

    add with v4.4.7

--- nvim/lua/hamu/plugins/noice.lua
+++ nvim/lua/hamu/plugins/noice.lua
@@ -1,35 +1,41 @@
 return
 {
-  -- "folke/noice.nvim",
-  -- event = "VeryLazy",
-  -- opts = {
-  --   -- add any options here
-  -- },
-  -- dependencies = {
-  --   "MunifTanjim/nui.nvim",
-  --   "rcarriga/nvim-notify",
-  -- },
-  -- config = function()
-  --   require("notify").setup({
-  --     background_colour = "#000000",
-  --   })
-  --   require("noice").setup({
-  --     lsp = {
-  --       -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-  --       override = {
-  --         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-  --         ["vim.lsp.util.stylize_markdown"] = true,
-  --         ["cmp.entry.get_documentation"] = true,
-  --       },
-  --     },
-  --     -- you can enable a preset for easier configuration
-  --     presets = {
-  --       bottom_search = false,        -- use a classic bottom cmdline for search
-  --       command_palette = false,      -- position the cmdline and popupmenu together
-  --       long_message_to_split = true, -- long messages will be sent to a split
-  --       inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-  --       lsp_doc_border = false,       -- add a border to hover docs and signature help
-  --     },
-  --   })
-  -- end
+  "folke/noice.nvim",
+  -- Apply 4.4.7
+  -- 4.5.0 has bug: "Cursor position outside buffer stack traceback"
+  commit = "d9328ef903168b6f52385a751eb384ae7e906c6f",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+  config = function()
+    require("notify").setup({
+      background_colour = "#000000",
+    })
+    require("noice").setup({
+      health = {
+        checker = true,
+      },
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = false,        -- use a classic bottom cmdline for search
+        command_palette = false,      -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false,       -- add a border to hover docs and signature help
+      },
+    })
+  end
 }
