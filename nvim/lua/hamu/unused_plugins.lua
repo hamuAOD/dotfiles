ae31cf9d3ae9145667e6f8acb86cf0605abb874f
hamuAOD <hamu.555to666@gmail.com>
2024-08-27 14:00:08 +0900

    delete vim-easy-align

--- nvim/lua/hamu/unused_plugins.lua
+++ nvim/lua/hamu/unused_plugins.lua
@@ -1,21 +1,36 @@
--- FZF 2024.08.27
 return {
-  "ibhagwan/fzf-lua",
-  -- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  event = "VeryLazy",
-  config = function()
-    -- calling `setup` is optional for customization
-    require"fzf-lua".setup({"fzf-native",winopts={preview={default="bat"}}})
+  -- FZF 2024.08.27
+  {
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
+    config = function()
+      -- calling `setup` is optional for customization
+      require"fzf-lua".setup({"fzf-native",winopts={preview={default="bat"}}})
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
-    local NS = { noremap = true, silent = true }
+      -- set keymaps
+      local keymap = vim.keymap -- for conciseness
+      local NS = { noremap = true, silent = true }
 
-    keymap.set('n', '<leader>ff', "<cmd>lua require('fzf-lua').files()<CR>", {desc = "Find files"}, NS)
-    keymap.set('n', '<leader>fl', "<cmd>lua require('fzf-lua').live_grep()<CR>", {desc = "Live Grep"}, NS)
-    keymap.set('n', '<leader>fr', "<cmd>lua require('fzf-lua').oldfiles()<CR>", {desc = "Recent Files"}, NS)
-    keymap.set('n', '<leader>fb', "<cmd>lua require('fzf-lua').buffers()<CR>", {desc = "Buffers"}, NS)
-    keymap.set('n', '<leader>fr', "<cmd>lua require('fzf-lua').registers()<CR>", {desc = "Registers"}, NS)
-  end,
+      keymap.set('n', '<leader>ff', "<cmd>lua require('fzf-lua').files()<CR>", {desc = "Find files"}, NS)
+      keymap.set('n', '<leader>fl', "<cmd>lua require('fzf-lua').live_grep()<CR>", {desc = "Live Grep"}, NS)
+      keymap.set('n', '<leader>fr', "<cmd>lua require('fzf-lua').oldfiles()<CR>", {desc = "Recent Files"}, NS)
+      keymap.set('n', '<leader>fb', "<cmd>lua require('fzf-lua').buffers()<CR>", {desc = "Buffers"}, NS)
+      keymap.set('n', '<leader>fr', "<cmd>lua require('fzf-lua').registers()<CR>", {desc = "Registers"}, NS)
+    end,
+  },
+  -- vim-easy-align 2024.08.27
+  {
+    'junegunn/vim-easy-align',
+    event = { "BufRead", "BufNewFile" },
+
+    config = function()
+      local keymap = vim.keymap
+      local NS = { noremap = true, silent = true }
+
+      keymap.set('n', '<Leader>ae', '<Plug>(EasyAlign)', {desc = "Easy-Align"}, NS)
+      keymap.set('x', '<Leader>ae', '<Plug>(EasyAlign)', {desc = "Easy-Align"}, NS)
+    end,
+  },
 }
