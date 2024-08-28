9aaca4a92cfe468861eb9669b54e80f77f6c13fc
hamuAOD <hamu.555to666@gmail.com>
2024-08-29 02:07:36 +0900

    change Lazy

--- nvim/lua/hamu/plugins/bufferline.lua
+++ nvim/lua/hamu/plugins/bufferline.lua
@@ -1,6 +1,6 @@
 return {
   'akinsho/bufferline.nvim',
-  event = "VeryLazy",
+  event = { "BufRead", "BufNewFile" },
   version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons',
 
--- nvim/lua/hamu/plugins/lazygit.lua
+++ nvim/lua/hamu/plugins/lazygit.lua
@@ -1,5 +1,6 @@
 return {
   "kdheepak/lazygit.nvim",
+  event = { "BufRead", "BufNewFile" },
   cmd = {
     "LazyGit",
     "LazyGitConfig",
