981d5c6a05bde5a0c7534c3da91b93eb50a454f7
hamuAOD <hamu.555to666@gmail.com>
2024-08-29 11:41:58 +0900

    add Lazy setting

--- nvim/lua/hamu/plugins/telescope.lua
+++ nvim/lua/hamu/plugins/telescope.lua
@@ -31,6 +31,7 @@ return {
   },
   {
     "danielfalk/smart-open.nvim",
+    event = "VeryLazy",
     branch = "0.2.x",
     config = function()
       require("telescope").load_extension("smart_open")
