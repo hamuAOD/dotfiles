470ca84b2b02b1485601ecf22ec1dd090574f33b
hamuAOD <hamu.555to666@gmail.com>
2024-08-30 15:01:54 +0900

    add mini.surround

--- nvim/lua/hamu/plugins/mini.lua
+++ nvim/lua/hamu/plugins/mini.lua
@@ -5,12 +5,20 @@ return
   event = "VeryLazy",
   config = function()
     require("mini.ai").setup()
+
     require("mini.align").setup({
       mappings = {
         start = 'ga',
         start_with_preview = 'gA',
       },
     })
-    require('mini.cursorword').setup()
+
+    require('mini.cursorword').setup({
+      vim.cmd [[highlight MiniCursorword guibg=darkcyan guifg=lightgray]]
+    })
+
+    require('mini.surround').setup({
+      custom_surroundings = nil,
+    })
   end,
 }
