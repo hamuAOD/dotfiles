c542f925efa4e32f2d43a85a1107492da4081f40
hamuAOD <hamu.555to666@gmail.com>
2024-09-03 13:39:09 +0900

    add mini.surround

--- nvim/lua/hamu/plugins/mini.lua
+++ nvim/lua/hamu/plugins/mini.lua
@@ -16,5 +16,20 @@ return
     require('mini.cursorword').setup({
       vim.cmd [[highlight MiniCursorword guibg=darkcyan guifg=lightgray]]
     })
+
+    require('mini.surround').setup({
+      mappings = {
+        add = 'sa', -- Add surrounding in Normal and Visual modes
+        delete = 'sd', -- Delete surrounding
+        find = 'sf', -- Find surrounding (to the right)
+        find_left = 'sF', -- Find surrounding (to the left)
+        highlight = 'sh', -- Highlight surrounding
+        replace = 'sr', -- Replace surrounding
+        update_n_lines = 'sn', -- Update `n_lines`
+
+        suffix_last = 'l', -- Suffix to search with "prev" method
+        suffix_next = 'n', -- Suffix to search with "next" method
+      },
+    })
   end,
 }
