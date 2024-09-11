8e9f28b59c5ca8c30618815e784a3f05ab1abdc8
hamuAOD <hamu.555to666@gmail.com>
2024-09-11 17:13:12 +0900

    update zen-mode

--- nvim/lua/hamu/plugins/zen-mode.lua
+++ nvim/lua/hamu/plugins/zen-mode.lua
@@ -2,9 +2,12 @@ return {
   "folke/zen-mode.nvim",
   event = "VeryLazy",
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    plugins = {
+      wezterm = {
+        enabled = true,
+        font = "+4",
+      },
+    },
   },
   config = function ()
     vim.keymap.set('n', '<Leader>z', '<CMD>ZenMode<CR>', {desc = "ZenMode"}, { noremap = true, silent = true })
