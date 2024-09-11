7a3bdf8747c24cf9364b689dd47eec97f6f92a7e
hamuAOD <hamu.555to666@gmail.com>
2024-09-11 17:12:52 +0900

    update

--- nvim/lua/hamu/plugins/telescope.lua
+++ nvim/lua/hamu/plugins/telescope.lua
@@ -50,10 +50,10 @@ return {
   },
   {
     "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-      "nvim-lua/plenary.nvim"
-    },
+    -- dependencies = {
+    --   "nvim-telescope/telescope.nvim",
+    --   "nvim-lua/plenary.nvim"
+    -- },
     event = "VeryLazy",
     config = function ()
       require("telescope").load_extension "file_browser"
