927ef22b6838b9a64195b99c96916df9576918c9
hamuAOD <hamu.555to666@gmail.com>
2024-09-17 13:01:28 +0900

    add search mode & change keymap

--- nvim/lua/hamu/plugins/flash.lua
+++ nvim/lua/hamu/plugins/flash.lua
@@ -6,15 +6,18 @@ return {
     modes = {
       char = {
         jump_labels = true
-      }
-    }
+      },
+      search = {
+        enabled = true
+      },
+    },
   },
   -- stylua: ignore
   keys = {
     { "m", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     -- { "<Leader>l", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "<Leader>L", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump( end, desc = "Flash" },
+    -- { "<Leader>L", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     -- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
