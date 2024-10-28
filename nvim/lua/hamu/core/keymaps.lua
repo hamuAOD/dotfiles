9730861397f12587fb38f3ee8d33b474906594cf
hamuAOD <hamu.555to666@gmail.com>
2024-10-28 13:39:25 +0900

    add keymap for substitute

--- nvim/lua/hamu/core/keymaps.lua
+++ nvim/lua/hamu/core/keymaps.lua
@@ -12,7 +12,7 @@ keymap.set('n', 'U', '<c-r>', NS)     -- リドゥ
 keymap.set('n', 'M', '%', NS)         -- 括弧ジャンプ
 keymap.set('x', 'M', '%', NS)         -- 括弧ジャンプ
 keymap.set('n', '<CR>', 'o<ESC>', NS) -- 改行だけ挿入
-keymap.set('n', 'gg', '0gg', NS)                  -- Vim Styleの先頭へ移動
+keymap.set('n', 'gg', '0gg', NS)      -- Vim Styleの先頭へ移動
 
 -- CUEファイル用検索
 keymap.set('n', '<Leader>c', '/\\( \\l\\|[^\\x01-\\x7E]\\|TITLE\\)<CR>', {desc = "Search: Check CUE"}, NS)
@@ -26,7 +26,10 @@ keymap.set('n', '>', '<C-w>><C-w>', NS)
 -- Current Directoryをファイルの場所にを変更
 keymap.set('n', 'cd', '<CMD>cd %:h<CR>', {desc = "Change Current Directory"}, NS)
 -- カーソル下のキーワードを置換
-keymap.set('n', 'S', ':%s/\\V\\<<C-r><C-w>\\>//g<Left><Left>', {remap = true})
+keymap.set('n', 'R', ':%s/\\V\\<<C-r><C-w>\\>//ge<Left><Left><Left>', {remap = true})
+-- 選択したキーワードを置換
+-- keymap.set('x', 'S', '"zy:%s/\\V<C-r><C-r>=escape\\(@z,'\\/\\\'\\)<CR>//gce<Left><Left><Left><Left>', {remap = true})
+keymap.set('x', 'R', '"zy:%s/\\V<C-r><C-r>=escape(@z,\' /\\\')<CR>//ge<Left><Left><Left>', {remap = true})
 -- ペースト結果のインデントを自動で揃える
 keymap.set('n', 'p', ']p`]', NS)
 keymap.set('n', 'P', ']P`]', NS)
