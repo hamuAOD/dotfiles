2f5dddd728ad0fe83817b2c1967a7ef1eb8d3710
hamuAOD <hamu.555to666@gmail.com>
2024-11-20 17:32:39 +0900

    cleanup

--- setup.sh
+++ setup.sh
@@ -4,13 +4,14 @@ directory="/Users/hamu"
 
 ln -sf "$directory/dotfiles/.vimrc" "$directory/.vimrc"
 ln -sf "$directory/dotfiles/.gvimrc" "$directory/.gvimrc"
+ln -sf "$directory/dotfiles/nvim/syntax" "$directory/.vim/syntax"
+ln -sf "$directory/dotfiles/nvim" "$directory/.config/nvim"
+
 ln -sf "$directory/dotfiles/.zshrc" "$directory/.zshrc"
 ln -sf "$directory/dotfiles/.zshenv" "$directory/.zshenv"
 ln -sf "$directory/dotfiles/wezterm" "$directory/.config/wezterm"
-#ln -sf "$directory/dotfiles/.vim" "$directory/.vim"
-ln -sf "$directory/dotfiles/nvim" "$directory/.config/nvim"
+
 ln -sf "$directory/dotfiles/.warp" "$directory/.warp"
-ln -sf "$directory/dotfiles/.tmux.conf" "$directory/.tmux.conf"
 ln -sf "$directory/dotfiles/alacritty" "$directory/.config/alacritty"
 ln -sf "$directory/dotfiles/yazi" "$directory/.config/yazi"
 ln -sf "$directory/dotfiles/karabiner.json" "$directory/.config/karabiner/karabiner.json"
