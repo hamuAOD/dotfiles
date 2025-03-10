21462f3d9fa3b08ec05ef15652354b1c6c57c49f
hamuAOD <hamu.555to666@gmail.com>
2025-03-10 18:59:41 +0900

    switch to macism

--- nvim/lua/hamu/plugins/im-select.lua
+++ nvim/lua/hamu/plugins/im-select.lua
@@ -4,12 +4,14 @@ return{
   config = function()
     require("im_select").setup({
       default_im_select = "com.apple.keylayout.ABC",
-      default_command = "/opt/homebrew/bin/im-select",
+      default_command = "/opt/homebrew/bin/macism",
     })
   end,
 }
 -- Install im-select
 -- brew tap daipeihust/tap && brew install im-select
+-- Install macism
+-- brew tap laishulu/homebrew && brew install macism
 -- [List]
 -- com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese
 -- com.apple.inputmethod.Kotoeri.RomajiTyping.Roman'
