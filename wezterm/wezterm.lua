05a4be8065fff902a6a18f4343b980cecdeb4d74
hamuAOD <hamu.555to666@gmail.com>
2025-03-11 13:09:01 +0900

    change line_height

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -25,10 +25,10 @@ wezterm.on('gui-startup', function(cmd)
 
 config.initial_cols = 130
 config.initial_rows = 50
-config.font = wezterm.font('HackGen35 Console NF', { weight = 'Bold', italic = false })
+config.font = wezterm.font('HackGen35 Console NF', { weight = "Bold", italic = false })
 -- config.font = wezterm.font('UDEV Gothic 35NFLG', { weight = 'Bold', italic = false })
 config.font_size = 13
-config.line_height = 1.15
+config.line_height = 1.20
 config.cell_width = 1.0
 config.window_padding = {
   left = '1.1cell',
