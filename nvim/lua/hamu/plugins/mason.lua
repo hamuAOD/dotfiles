3f1c78b832bc566039288d8ff7ee631c745c4b6d
hamuAOD <hamu.555to666@gmail.com>
2025-05-31 03:00:51 +0900

    Neovim-Mason: fix event - Mason command available at startup

--- nvim/lua/hamu/plugins/mason.lua
+++ nvim/lua/hamu/plugins/mason.lua
@@ -5,7 +5,7 @@ return {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     "neovim/nvim-lspconfig",
   },
-  event = "BufReadPre",
+  event = "VeryLazy",
 
   config = function()
     local servers = {
