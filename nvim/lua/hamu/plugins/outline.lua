f01ac6b6ad277f4166e69e4d7f95965a64bd1033
hamuAOD <htaedlegna.86@gmail.com>
2025-06-17 14:13:20 +0900

    Neoim: add autofold_depth

--- nvim/lua/hamu/plugins/outline.lua
+++ nvim/lua/hamu/plugins/outline.lua
@@ -7,7 +7,9 @@ return
     vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
 
     require("outline").setup {
-      -- Your setup opts here (leave empty to use defaults)
+      symbol_folding = {
+        autofold_depth = false,
+      }
     }
   end,
 }
