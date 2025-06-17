52192fd2352eb4ab1ca0667f1c8b6c820370ae05
hamuAOD <htaedlegna.86@gmail.com>
2025-06-17 21:09:18 +0900

    Neovim: outline.nvim set provider_selector

--- nvim/lua/hamu/plugins/outline.lua
+++ nvim/lua/hamu/plugins/outline.lua
@@ -7,6 +7,12 @@ return
     vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
 
     require("outline").setup {
+      provider_selector = function(bufnr, filetype, buftype)
+        if filetype == 'swift' then
+          return {'lsp', 'treesitter'}
+        end
+        return {'lsp', 'treesitter'}
+      end,
       symbol_folding = {
         autofold_depth = false,
       }
