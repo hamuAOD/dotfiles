980c16c0e8fb60c4ada319bc35e1ffe673485a4c
hamuAOD <htaedlegna.86@gmail.com>
2025-09-25 16:58:52 +0900

    Neovim: [nvim-cmp] add Snippets & Dictionary

--- nvim/lua/hamu/plugins/cmp-nvim.lua
+++ nvim/lua/hamu/plugins/cmp-nvim.lua
@@ -1,6 +1,9 @@
 return {
   "hrsh7th/nvim-cmp",
   dependencies = {
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "uga-rosa/cmp-dictionary",
     'windwp/nvim-autopairs',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
@@ -11,7 +14,9 @@ return {
 
   config = function()
     local cmp = require("cmp")
+    local luasnip = require("luasnip")
     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+
     local default_matching = {
       disallow_partial_matching = false,
       disallow_prefix_unmatching = false,
@@ -20,8 +25,54 @@ return {
       ignore_case = true,
     }
 
+    local dict = {
+      ["*"] = { "$HOME/.config/nvim/lua/hamu/dict/all.dict" },
+      ft = {
+        vhdl = { "$HOME/.config/nvim/lua/hamu/dict/vhdl.dict" },
+      },
+    }
+
+    -- Lua スニペットを読み込む
+    require("luasnip.loaders.from_lua").lazy_load({
+      paths = vim.fn.stdpath("config") .. "/lua/hamu/snippets"
+    })
+    -- VSCode 形式のスニペットを読み込む
+    require("luasnip.loaders.from_vscode").lazy_load({
+      paths = { vim.fn.stdpath("config") .. "/lua/hamu/snippets" }
+      -- paths = "$HOME/.config/nvim/lua/hamu/snippets"
+    })
+    -- cmp-dictionary
+    require("cmp_dictionary").setup({
+      paths = dict["*"],
+      exact_length = 1,
+      first_case_insensitive = true,
+      -- document = {
+      --   enable = true,
+      --   command = { "wn", "${label}", "-over" },
+      -- },
+    })
+
+    vim.api.nvim_create_autocmd("BufEnter", {
+      pattern = "*",
+      callback = function(ev)
+        local paths = dict.ft[vim.bo.filetype] or {}
+        vim.list_extend(paths, dict["*"])
+        require("cmp_dictionary").setup({
+          paths = paths,
+        })
+      end
+    })
+
     -- Insert モード補完
     cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      completion = {
+        autocomplete = { cmp.TriggerEvent.TextChanged }
+      },
       mapping = cmp.mapping.preset.insert({
         ["<Tab>"]   = cmp.mapping.select_next_item(),
         ["<S-Tab>"] = cmp.mapping.select_prev_item(),
@@ -30,11 +81,22 @@ return {
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"]   = cmp.mapping.abort(),
         ["<CR>"]    = cmp.mapping.confirm({ select = true }),
+        ["<C-s>"]   = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
       }),
       sources = cmp.config.sources({
+        { name = "luasnip" },
         { name = "nvim_lsp" },
-        { name = "buffer", keyword_length = 1, max_item_count = 10 },
+        { name = "buffer", keyword_length = 1, max_item_count = 10, },
         { name = "path" },
+        { name = "dictionary", keyword_length = 1, },
       }),
       window = {
         completion = cmp.config.window.bordered(),
