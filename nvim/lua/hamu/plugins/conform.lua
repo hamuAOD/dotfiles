f26678cb9f77390813024be178e67a4875f3e6e0
hamuAOD <htaedlegna.86@gmail.com>
2025-09-30 16:13:42 +0900

    Neovim: [conform] add some formats

--- nvim/lua/hamu/plugins/conform.lua
+++ nvim/lua/hamu/plugins/conform.lua
@@ -1,27 +1,33 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {},
-  event = { "BufRead", "BufNewFile" },
+	"stevearc/conform.nvim",
+	opts = {},
+	event = { "BufRead", "BufNewFile" },
 
-  config = function()
-    require("conform").setup({
-      formatters_by_ft = {
-        -- lua = { "stylua" },
-        lua = { "lua_ls" },
-        c = { "clang_format" },
-        cpp = { "clang_format" },
-        python = { "isort", "black" },
-        rust = { "rustfmt", lsp_format = "fallback" },
-        javascript = { "prettierd", "prettier", stop_after_first = true },
-        yaml = { "yamlfmt", "prettier", "yamlfix" },
-        vhdl = { "vsg" },
-      },
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "lua_ls", "stylua" },
+				sh = { "shfmt" },
+				bash = { "shfmt" },
+				zsh = { "shfmt" },
+				markdown = { "prettier" },
+				swift = { "swift-format" },
+				c = { "clang_format" },
+				cpp = { "clang_format" },
+				go = { "gopls" },
+				python = { "isort", "black" },
+				rust = { "rustfmt", lsp_format = "fallback" },
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				yaml = { "yamlfmt", "prettier", "yamlfix" },
+				vhdl = { "vsg" },
+			},
 
-      vim.keymap.set("n", "<leader>v", function()
-        require("conform").format({ async = true, lsp_fallback = true })
-      end, { desc = "Format file" }),
-    })
-  end,
+			vim.keymap.set("n", "<leader>v", function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end, { desc = "Format file" }),
+		})
+	end,
 }
 -- for Mac
--- brew install stylua prettier clang-format
+-- brew install stylua prettier clang-format shfmt swift-format
+-- pip install vsg
