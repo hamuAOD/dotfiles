7671fe17c28e4eb0e244d06982952977dc223a67
hamuAOD <htaedlegna.86@gmail.com>
2025-10-09 15:36:31 +0900

    Neovim: [flash] restore default 's' behavior in visual mode

--- nvim/lua/hamu/plugins/flash.lua
+++ nvim/lua/hamu/plugins/flash.lua
@@ -1,26 +1,32 @@
 return {
-  "folke/flash.nvim",
-  event = { "BufRead", "BufNewFile" },
-  ---@type Flash.Config
-  opts = {
-    modes = {
-      char = {
-        jump_labels = true
-      },
-      search = {
-        enabled = false
-      },
-    },
-  },
-  -- stylua: ignore
-  keys = {
-    { ";", mode = { "n", "x", "o" }, function() require("flash").jump() end, {desc = "Flash",noremap = true} },
+	"folke/flash.nvim",
+	event = { "BufRead", "BufNewFile" },
+	---@type Flash.Config
+	opts = {
+		modes = {
+			char = {
+				jump_labels = true,
+			},
+			search = {
+				enabled = false,
+			},
+		},
+	},
+
+	keys = {
+		{ ";", mode = { "n", "x", "o" }, function() require("flash").jump() end, { desc = "Flash", noremap = true }, },
     -- { "<Leader>l", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     -- { "<Leader>L", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     -- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  },
+		{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search", },
+	},
+
+	config = function(_, opts)
+		require("flash").setup(opts)
+		-- flash.nvim の読み込み後に s をデフォルトの動作に戻す
+		vim.keymap.set({ "n", "x" }, "s", "s", { noremap = true })
+	end,
 }
