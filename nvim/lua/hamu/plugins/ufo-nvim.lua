f90c0d153b4fb71566b388162e70fe0ad1d555cc
hamuAOD <htaedlegna.86@gmail.com>
2025-11-16 18:44:35 +0900

    Neovim: change event

--- nvim/lua/hamu/plugins/ufo-nvim.lua
+++ nvim/lua/hamu/plugins/ufo-nvim.lua
@@ -1,7 +1,7 @@
 return {
   "kevinhwang91/nvim-ufo",
   dependencies = { "kevinhwang91/promise-async" },
-  event = "VeryLazy",
+  event = { "BufRead", "BufNewFile" },
 
   opts = function()
     vim.o.foldcolumn = '1' -- '0' is not bad
