2c87bce3a750ba9d8b8f8a0e60f2b8126dfb8d5e
hamuAOD <htaedlegna.86@gmail.com>
2026-07-03 02:07:02 +0900

    Wezterm: change Leader-key

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -38,7 +38,7 @@ config.window_padding = {
   top = '0.5cell',
   bottom = '0.5cell',
 }
-config.window_background_opacity = 0.89
+config.window_background_opacity = 0.85
 config.macos_window_background_blur = 12
 -- config.treat_east_asian_ambiguous_width_as_wide = true
 config.window_close_confirmation = 'NeverPrompt'  -- AlwaysPrompt
@@ -49,7 +49,7 @@ config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 -- "TITLE", "RESIZE", "TITLE | RESIZE"
-config.window_decorations = "TITLE | RESIZE"
+config.window_decorations = "RESIZE"
 
 --config.background = {
 --  -- This is the deepest/back-most layer. It will be rendered first
@@ -83,7 +83,8 @@ config.window_decorations = "TITLE | RESIZE"
 
 -- Key Configs
 -- config.disable_default_key_bindings = true
-config.leader = { key = ':', mods = 'CTRL|SHIFT', timeout_milliseconds = 1000 }
+-- config.leader = { key = ':', mods = 'CTRL|SHIFT', timeout_milliseconds = 1000 }
+config.leader = { key = "'", mods = 'CTRL', timeout_milliseconds = 1000 }
 
 config.keys = {
   {
