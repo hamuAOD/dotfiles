f4d73e1fe9923ad521c0e3c55fd641c1c8485674
hamuAOD <htaedlegna.86@gmail.com>
2026-07-25 02:25:02 +0900

    Revise symlink setup and remove cleanup script

--- setup.sh
+++ setup.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
+LNOPT="-snv"
+# LNOPT="-snvf"
+
 ### Zsh
-ln -sfv "$HOME/.dotfiles/zsh/.zshrc" "$HOME/.zshrc"
-ln -sfv "$HOME/.dotfiles/zsh/.zshenv" "$HOME/.zshenv"
+ln $LM   "$HOME/.dotfiles/zsh/.zshrc" "$HOME/.zshrc"
+ln $LM   "$HOME/.dotfiles/zsh/.zshenv" "$HOME/.zshenv"
 ### Zsh for Ubuntu
-# ln -sfv "$HOME/.dotfiles/zsh/ubuntu.zshrc" "$HOME/.zshrc"
-# ln -sfv "$HOME/.dotfiles/zsh/ubuntu.zshenv" "$HOME/.zshenv"
+# ln $LNOPT "$HOME/.dotfiles/zsh/ubuntu.zshrc" "$HOME/.zshrc"
+# ln $LNOPT "$HOME/.dotfiles/zsh/ubuntu.zshenv" "$HOME/.zshenv"
 ### VIm
-ln -sfv "$HOME/.dotfiles/.vimrc" "$HOME/.vimrc"
-ln -sfv "$HOME/.dotfiles/.gvimrc" "$HOME/.gvimrc"
-ln -sfv "$HOME/.dotfiles/nvim" "$HOME/.config/nvim"
-ln -sfv "$HOME/.dotfiles/nvim/syntax" "$HOME/.vim/syntax"
-ln -sfv "$HOME/.dotfiles/nvim" "$HOME/.config/nvim"
+ln $LNOPT "$HOME/.dotfiles/.vimrc" "$HOME/.vimrc"
+ln $LNOPT "$HOME/.dotfiles/.gvimrc" "$HOME/.gvimrc"
+ln $LNOPT "$HOME/.dotfiles/nvim" "$HOME/.config/nvim"
+ln $LNOPT "$HOME/.dotfiles/nvim/syntax" "$HOME/.vim/syntax"
+ln $LNOPT "$HOME/.dotfiles/nvim" "$HOME/.config/nvim"
 ### etc
-ln -sfv "$HOME/.dotfiles/ssh/config" "$HOME/.ssh/"
+ln $LNOPT "$HOME/.dotfiles/ssh/config" "$HOME/.ssh/"
 
 ### ~/.config : directorys
-ln -sfv "$HOME/.dotfiles/borders" "$HOME/.config/borders"
-ln -sfv "$HOME/.dotfiles/ghostty" "$HOME/.config/ghostty"
-ln -sfv "$HOME/.dotfiles/goneovim" "$HOME/.config/goneovim"
-ln -sfv "$HOME/.dotfiles/karabiner/complex_modifications" "$HOME/.config/karabiner/assets/complex_modifications"
-ln -sfv "$HOME/.dotfiles/tmux/scripts" "$HOME/.config/tmux/scripts"
-ln -sfv "$HOME/.dotfiles/wezterm" "$HOME/.config/wezterm"
-ln -sfv "$HOME/.dotfiles/yazi" "$HOME/.config/yazi"
-ln -sfv "$HOME/.dotfiles/zonvie" "$HOME/.config/zonvie"
+ln $LNOPT "$HOME/.dotfiles/borders" "$HOME/.config/borders"
+ln $LNOPT "$HOME/.dotfiles/ghostty" "$HOME/.config/ghostty"
+ln $LNOPT "$HOME/.dotfiles/goneovim" "$HOME/.config/goneovim"
+ln $LNOPT "$HOME/.dotfiles/karabiner/complex_modifications" "$HOME/.config/karabiner/assets/complex_modifications"
+ln $LNOPT "$HOME/.dotfiles/wezterm" "$HOME/.config/wezterm"
+ln $LNOPT "$HOME/.dotfiles/yazi" "$HOME/.config/yazi"
+ln $LNOPT "$HOME/.dotfiles/zonvie" "$HOME/.config/zonvie"
+ln $LNOPT "$HOME/.dotfiles/lazygit" "$HOME/.config/lazygit"
+ln $LNOPT "$HOME/.dotfiles/superfile" "$HOME/.config/superfile"
 ### ~/.config : files
-ln -sfv "$HOME/.dotfiles/goneovim/settings.toml" "$HOME/.config/goneovim/settings.toml"
-ln -sfv "$HOME/.dotfiles/herdr/config.toml" "$HOME/.config/herdr/config.toml"
-ln -sfv "$HOME/.dotfiles/starship/starship.toml" "$HOME/.config/starship.toml"
-ln -sfv "$HOME/.dotfiles/tmux/tmux.conf" "$HOME/.config/tmux/tmux.conf"
+ln $LNOPT "$HOME/.dotfiles/goneovim/settings.toml" "$HOME/.config/goneovim/settings.toml"
+ln $LNOPT "$HOME/.dotfiles/herdr/config.toml" "$HOME/.config/herdr/config.toml"
+ln $LNOPT "$HOME/.dotfiles/starship/starship.toml" "$HOME/.config/starship.toml"
+ln $LNOPT "$HOME/.dotfiles/tmux/scripts" "$HOME/.config/tmux/scripts"
+ln $LNOPT "$HOME/.dotfiles/tmux/tmux.conf" "$HOME/.config/tmux/tmux.conf"
