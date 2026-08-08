3971a9f2ab7c4231c1c732afb20118a4f2c34233
hamuAOD <106303202+hamuAOD@users.noreply.github.com>
2026-08-09 04:38:41 +0900

    Move im-select.nvim configuration to unused plugins

--- nvim/lua/hamu/unused_plugins.lua
+++ nvim/lua/hamu/unused_plugins.lua
@@ -970,4 +970,32 @@ return {
       end,
     },
   },
+  {
+    "keaising/im-select.nvim",
+    cond = function()
+      local os = vim.loop.os_uname().sysname
+      return os == "Darwin"  -- Mac なら true（ロードされる）、Ubuntu なら false（ロードされない）
+    end,
+    event = { "BufRead", "BufNewFile" },
+    config = function()
+      require("im_select").setup({
+        default_im_select = "com.apple.keylayout.ABC",
+        default_command = "/opt/homebrew/bin/macism",
+
+        -- 以下のイベント時に、デフォルトのIMEになる
+        set_default_events  = { "VimEnter", "InsertEnter", "InsertLeave" },
+
+        -- 以下のイベント時に、前回使われていたIMEになる（無効にしている）
+        set_previous_events = {},
+      })
+    end,
+    -- Install im-select
+    -- brew tap daipeihust/tap && brew install im-select
+    -- Install macism
+    -- brew tap laishulu/homebrew && brew install macism
+    -- [List]
+    -- com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese
+    -- com.apple.inputmethod.Kotoeri.RomajiTyping.Roman'
+    -- com.apple.keylayout.ABC
+  },
 }
