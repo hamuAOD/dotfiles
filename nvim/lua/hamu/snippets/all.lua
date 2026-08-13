d4809a1a07cd505bdc4cdc0db2d01c27ce7df627
hamuAOD <106303202+hamuAOD@users.noreply.github.com>
2026-08-14 04:17:16 +0900

    Enable autosnippets and add shebang snippet

--- nvim/lua/hamu/snippets/all.lua
+++ nvim/lua/hamu/snippets/all.lua
@@ -3,9 +3,19 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
+ls.config.setup({
+  enable_autosnippets = true,
+})
+
 return {
   -- entity スニペット
   s("ltest", {
     t("lua test!! "),
+  }),
+  s({
+    trig = "shebang;",
+    snippetType = "autosnippet",
+  }, {
+    t("#!/usr/bin/env bash"),
   }),
 }
