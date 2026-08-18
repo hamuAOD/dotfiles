ac9bc6f2ec9dc4afc436d930103a1dbf081409ff
hamuAOD <106303202+hamuAOD@users.noreply.github.com>
2026-08-18 16:40:23 +0900

    Enable Snacks dashboard and expand picker keymaps

--- nvim/lua/hamu/plugins/snacks.lua
+++ nvim/lua/hamu/plugins/snacks.lua
@@ -1,10 +1,31 @@
+local version = vim.version()
+local version_text = ("v%d.%d.%d"):format(
+  version.major,
+  version.minor,
+  version.patch
+)
+
+local function fmt_logo(left, right, padding)
+  return {
+    text = {
+      { left,  hl = "DashboardLogoBlue" },
+      { right, hl = "DashboardLogoCyan" },
+    },
+    align = "center",
+    padding = padding,
+  }
+end
+
 return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
   ---@type snacks.Config
+
   opts = {
     scroll = { enabled = true },
+    bigfile = { enabled = true }, -- 巨大・圧縮済みファイルでTreesitterやLSPが固まるのを防ぐ
+    quickfile = { enabled = true }, -- nvim ファイル名で直接起動したとき、先にファイルを表示する
     terminal = {
       win = {
         position = "float",
@@ -14,53 +35,140 @@ return {
         title_pos = "center",
       },
     },
-    bigfile = { enabled = true }, -- 巨大・圧縮済みファイルでTreesitterやLSPが固まるのを防ぐ
-    quickfile = { enabled = true }, -- nvim ファイル名で直接起動したとき、先にファイルを表示する
-    dashboard = { enabled = false },
-    picker = { enabled = false }
-  },
-  keys = {
-    -- Find
-    { "<leader>b", function() Snacks.picker.buffers() end, desc = "Buffers", },
-    { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers", },
-    { "<leader>fg", function() Snacks.picker.grep() end, desc = "Grep", },
-    { "<leader>fh", function() Snacks.picker.command_history() end, desc = "Command History", },
-    { "<leader>fo", function() Snacks.picker.recent() end, desc = "Old Files", },
-    { "<leader>fr", function() Snacks.picker.registers() end, desc = "Registers", },
-    { "<leader>fk", function() Snacks.picker.keymaps() end, desc = "Keymaps", },
-    { "<leader>fl", function() Snacks.picker.lines() end, desc = "Grep Current Buffer Line", },
-    { "<leader>fB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers", },
-    { "<leader>fs", function() Snacks.picker.smart() end, desc = "Smart Find Files", },
-    { "<leader>fm", function() Snacks.picker.marks() end, desc = "Marks", },
-    { "<leader>fc", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
-    { "<leader>:", function() Snacks.picker.commands() end, desc = "Picker Commands" },
-    {
-      "<leader>ff",
-      function()
-        Snacks.picker.files({
-          cwd = vim.fn.expand("~"),
-          hidden = true,
-          ignored = true,
-        })
-      end,
-      desc = "Find Files",
+
+    keys = {
+      { "<Leader><Leader>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+      { "<leader>b", function() Snacks.picker.buffers() end, desc = "Buffers" },
+      { "<leader>:", function() Snacks.picker.commands() end, desc = "Picker Commands" },
+      -- Find
+      { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+      { "<leader>fB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
+      { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+      { "<leader>fC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
+      {
+        "<leader>ff",
+        function()
+          Snacks.picker.files({ cwd = vim.fn.expand("~"), hidden = true, ignored = true })
+        end,
+        desc = "Find Files",
+      },
+      { "<leader>fg", function() Snacks.picker.grep() end, desc = "Grep" },
+      { "<leader>fh", function() Snacks.picker.command_history() end, desc = "Command History" },
+      { "<leader>fi", function() Snacks.picker.icons() end, desc = "Icons" },
+      { "<leader>fk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
+      { "<leader>fl", function() Snacks.picker.lines() end, desc = "Grep Current Buffer Line" },
+      { "<leader>fm", function() Snacks.picker.marks() end, desc = "Marks" },
+      { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent Files" },
+      { "<leader>fR", function() Snacks.picker.registers() end, desc = "Registers" },
+      { "<leader>fs", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+      { "<leader>fu", function() Snacks.picker.undo() end, desc = "Undo History" },
+      -- Git
+      { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+      { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff" },
+      { "<leader>gf", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+      { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
+      { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
+      -- Other
+      { "<C-d>", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+      { "<c-\\>", function() Snacks.terminal() end, desc = "Toggle Terminal", mode = { "n", "t" } },
+      { "<leader>tz", function() Snacks.zen() end, desc = "Toggle Zen Mode" },
     },
-    -- Git
-    { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit", },
-    { "<leader>gf", function() Snacks.picker.git_files() end, desc = "Find Git Files", },
-    -- Other
-    { "<leader>tz", function() Snacks.zen() end, desc = "Toggle Zen Mode", },
-    { "<c-\\>", function() Snacks.terminal() end, desc = "Toggle Terminal", mode = { "n", "t" },
+
+    dashboard = {
+      enabled = true,
+
+      preset = {
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', { cwd = vim.fn.stdpath('config') })",
+          },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = "󰒲  ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+          { icon = "󰣪  ", key = "m", desc = "Mason", action = ":Mason" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+      sections = {
+        fmt_logo("│ ", "╲ ││"),
+        fmt_logo("││", "╲╲││"),
+        fmt_logo("││", " ╲ │", 1),
+
+        {
+          text = {
+            { "NVIM ", hl = "DashboardLogoCyan" },
+            { version_text, hl = "DiagnosticOk" },
+          },
+          align = "center",
+          padding = 1,
+        },
+        {
+          text = {
+            { string.rep("─", 60), hl = "Comment" },
+          },
+          align = "center",
+          padding = 1,
+        },
+        {
+          text = {
+            { "Nvim is open source and freely distributable", hl = "Normal" },
+          },
+          align = "center",
+        },
+        {
+          text = {
+            { "https://neovim.io/#chat", hl = "DashboardLogoCyan" },
+          },
+          align = "center",
+          padding = 1,
+        },
+        {
+          text = {
+            { string.rep("─", 60), hl = "Comment" },
+          },
+          align = "center",
+          padding = 1,
+        },
+
+        { section = "keys", gap = 1, padding = 1 },
+        {
+          icon = " ",
+          title = "Recent Files",
+          section = "recent_files",
+          limit = 5,
+          indent = 2,
+          padding = 1,
+        },
+        { section = "startup" },
+      },
     },
+    picker = { enabled = true },
   },
+
   init = function()
     vim.api.nvim_create_autocmd("User", {
       pattern = "VeryLazy",
       callback = function()
         Snacks.util.set_hl({
           SnacksPickerDir = "Normal",
+
+          SnacksDashboardDir = "Comment",
+          SnacksDashboardFile = "Special",
         })
       end,
+    })
+
+    vim.api.nvim_set_hl(0, "DashboardLogoBlue", {
+      fg = "#8be9fd",
+    })
+    vim.api.nvim_set_hl(0, "DashboardLogoCyan", {
+      fg = "#50fa7b",
     })
   end,
 }
