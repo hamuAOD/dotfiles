adcfee713a7af8794554b11ec99ec7f1590937ab
hamuAOD <106303202+hamuAOD@users.noreply.github.com>
2026-08-18 13:50:38 +0900

    Configure mini.bracketed with buffer and comment mappings disabled

--- nvim/lua/hamu/plugins/mini.lua
+++ nvim/lua/hamu/plugins/mini.lua
@@ -1,6 +1,5 @@
-return
-{
-  'echasnovski/mini.nvim',
+return {
+  "echasnovski/mini.nvim",
   version = false,
   event = "VeryLazy",
   config = function()
@@ -8,30 +7,49 @@ return
 
     require("mini.align").setup({
       mappings = {
-        start = 'ga',
-        start_with_preview = 'gA',
+        start = "ga",
+        start_with_preview = "gA",
       },
     })
 
     -- require('mini.completion').setup()
 
-    require('mini.cursorword').setup({
-      vim.cmd [[highlight MiniCursorword guibg=darkcyan guifg=lightgray]]
+    require("mini.cursorword").setup({
+      vim.cmd([[highlight MiniCursorword guibg=darkcyan guifg=lightgray]]),
     })
 
-    require('mini.surround').setup({
+    require("mini.surround").setup({
       mappings = {
-        add = 'sa', -- Add surrounding in Normal and Visual modes
-        delete = 'sd', -- Delete surrounding
-        find = 'sf', -- Find surrounding (to the right)
-        find_left = 'sF', -- Find surrounding (to the left)
-        highlight = 'sh', -- Highlight surrounding
-        replace = 'sr', -- Replace surrounding
-        update_n_lines = 'sn', -- Update `n_lines`
+        add = "sa",             -- Add surrounding in Normal and Visual modes
+        delete = "sd",          -- Delete surrounding
+        find = "sf",            -- Find surrounding (to the right)
+        find_left = "sF",       -- Find surrounding (to the left)
+        highlight = "sh",       -- Highlight surrounding
+        replace = "sr",         -- Replace surrounding
+        update_n_lines = "sn",  -- Update `n_lines`
 
-        suffix_last = 'l', -- Suffix to search with "prev" method
-        suffix_next = 'n', -- Suffix to search with "next" method
+        suffix_last = "l",      -- Suffix to search with "prev" method
+        suffix_next = "n",      -- Suffix to search with "next" method
       },
+    })
+
+    require('mini.bracketed').setup({
+      buffer     = { suffix = "" },
+      comment    = { suffix = "" },
+      -- buffer     = { suffix = 'b', options = {} },
+      -- comment    = { suffix = 'c', options = {} },
+      -- conflict   = { suffix = 'x', options = {} },
+      -- diagnostic = { suffix = 'd', options = {} },
+      -- file       = { suffix = 'f', options = {} },
+      -- indent     = { suffix = 'i', options = {} },
+      -- jump       = { suffix = 'j', options = {} },
+      -- location   = { suffix = 'l', options = {} },
+      -- oldfile    = { suffix = 'o', options = {} },
+      -- quickfix   = { suffix = 'q', options = {} },
+      -- treesitter = { suffix = 't', options = {} },
+      -- undo       = { suffix = 'u', options = {} },
+      -- window     = { suffix = 'w', options = {} },
+      -- yank       = { suffix = 'y', options = {} },
     })
   end,
 }
