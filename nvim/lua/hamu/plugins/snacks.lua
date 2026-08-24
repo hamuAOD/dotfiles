010309971d6ad1fe3cdc532c1c2efa31b49c8998
hamuAOD <106303202+hamuAOD@users.noreply.github.com>
2026-08-24 19:41:38 +0900

    Use smart picker for dashboard file search

--- nvim/lua/hamu/plugins/snacks.lua
+++ nvim/lua/hamu/plugins/snacks.lua
@@ -79,7 +79,7 @@ return {
 
       preset = {
         keys = {
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('smart')" },
           { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
           { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
           { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
